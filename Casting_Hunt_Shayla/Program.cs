4d1df8a3ff2764088ffe2c560c86a9c3d28ce167
Shayla <Shaylahunt834@gmail.com>
2025-03-07 15:15:50 -0600

    Update Program.cs

--- Casting_Hunt_Shayla/Program.cs
+++ Casting_Hunt_Shayla/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter a whole number!"); 
+            int wholeNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favoite number to concsole
+            Console.WriteLine("Your favorite number is " + wholeNum); 
+
+            // Asks user a ture or false question about videogames and assigns response to boolean
+            Console.WriteLine("Do you like video games? (Please enter 'true' or 'false')");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Print the result of the question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
         }
     }
 }
